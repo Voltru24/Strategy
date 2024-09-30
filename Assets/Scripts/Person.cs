d49ce48719c06e26908e41095e3680eec3c29510
Voltru24 <114227692+Voltru24@users.noreply.github.com>
2024-09-30 12:39:08 +0300

    1

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "new Person", menuName = "Person/Create new person",order = 52)]
 public class Person : ScriptableObject
 {
+    //---------
     [SerializeField] private string _name;
     [SerializeField] private Color _colorName;
 }
